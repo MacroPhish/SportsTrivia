3a79786ad7b5eddaa28c5f9ae0d2bb852d4a55c6
Your Name <zfish543@gmail.com>
2026-04-02 17:33:48 -0400

    Updates

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+// [REQUIRED] Firebase Messaging Service Worker
+importScripts('https://www.gstatic.com/firebasejs/10.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.0.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  // TODO: Replace with your Firebase config
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: '/icon-192.png' // Optional: path to your app icon
+  });
+});
